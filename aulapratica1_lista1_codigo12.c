69d0100fb6bc4c36c80a6ab4a08821d36bd4c7ab
pedrinndias <pedro3soares@gmail.com>
2025-03-14 23:52:26 -0300

    Questao 13 salario minimo

---
+++ aulapratica1_lista1_codigo12.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int hora, minutos, tempfinal;
+    printf("Escreva a hora e os minutos: ");
+    scanf("%d %d", &hora, &minutos);
+    tempfinal=hora*60+minutos;
+    printf("O tempo total em minutos eh: %d\n", tempfinal);
+    return 0;
+}
