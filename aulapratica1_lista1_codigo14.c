31ed8ae001d8d5647123312525e047aafe41063c
pedrinndias <pedro3soares@gmail.com>
2025-03-15 10:09:02 -0300

    correçãofinal

--- aulapratica1_lista1_codigo14.c
+++ aulapratica1_lista1_codigo14.c
@@ -3,11 +3,11 @@ int main(){
     int num, suc;
     printf("Escreva um numero entre 0 e 60: ");
     scanf("%d", &num);
-    if((num>=0)||(num<60)){
+    if((num>=0)&&(num<60)){
         suc=num+1;
         printf("O sucessor de %d e: %d\n", num, suc);
     }
-    else{ if(num==60){
+    else { if(num==60){
         printf("O numero e 60, nao ha sucessor\n");}
         else
         printf("O numero nao esta entre 0 e 60\n");
