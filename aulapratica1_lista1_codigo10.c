4b127eef79e410bef4f7fe8f45bd0c8fe3b11d7c
pedrinndias <pedro3soares@gmail.com>
2025-03-14 23:07:22 -0300

    Questao 10 A virando B e B virando A

---
+++ aulapratica1_lista1_codigo10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    float a, b, c;
+    printf(" Escreva dois numeros reais referentes a A e B: ");
+    scanf("%f %f", &a, &b);
+    a=c;
+    b=a;
+    c=b;
+    printf("A agora vale %f", a);
+    printf("B agora vale %f", b);
+    return 0;
+}
