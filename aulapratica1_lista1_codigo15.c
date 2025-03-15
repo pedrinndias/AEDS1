459248cf48750351d2c7e2cd48102a26f1f8f81e
pedrinndias <pedro3soares@gmail.com>
2025-03-15 11:19:32 -0300

    correção

--- aulapratica1_lista1_codigo15.c
+++ aulapratica1_lista1_codigo15.c
@@ -5,7 +5,7 @@ int main(){
    float p, i, rend;
    printf("Escreva o capital, a taxa de juros e o tempo: ");
     scanf("%f %f %d", &p, &i, &n);
-    rend=(p*((pow(1+i, n)-1)/i));
+    rend = p * ((pow(1+i, n)-1)/i);
     printf("O rendimento e: %.2f\n", rend);
     return 0;
 }
