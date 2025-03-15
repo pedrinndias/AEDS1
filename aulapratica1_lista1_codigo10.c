b757f7831e47ee49862b84b91741e76db42644e9
pedrinndias <pedro3soares@gmail.com>
2025-03-14 23:09:14 -0300

    correção questao 10

--- aulapratica1_lista1_codigo10.c
+++ aulapratica1_lista1_codigo10.c
@@ -3,9 +3,9 @@ int main (){
     float a, b, c;
     printf(" Escreva dois numeros reais referentes a A e B: ");
     scanf("%f %f", &a, &b);
-    a=c;
-    b=a;
-    c=b;
+    c=a;
+    a=b;
+    b=c;
     printf("A agora vale: %f", a);
     printf("B agora vale: %f", b);
     return 0;
