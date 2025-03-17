f277ed4c9e0255aa40249ca50e50920027916efc
pedrinndias <pedro3soares@gmail.com>
2025-03-17 18:59:26 -0300

    Questão 14 - soma numerador e ndenominador

---
+++ aulapratica4_lista4_codigo14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int denominador, n;
+    float soma, numerador;
+    printf("Escreva um numero inteiro: ");
+    scanf("%d", &n);
+    soma=0;
+    if(n==1){
+        soma=1;
+    } else for (int i = 1; i <= n; i++){
+        numerador = i;
+        denominador = i+1;
+        soma = soma + numerador/denominador;
+    }
+    printf("O valor da soma e: %f\n", soma);
+    return 0;
+}
