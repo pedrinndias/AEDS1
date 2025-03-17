b5885a055e7e5b057f85632fc49d9bbdd13d4f6b
pedrinndias <pedro3soares@gmail.com>
2025-03-17 18:04:57 -0300

    Questão 8 - Nota aluno

--- aulapratica4_lista4_codigo7.c
+++ aulapratica4_lista4_codigo7.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 int main(){
     float nota;
-    printf("Escreva a nota do aluno: ");
-    scanf("%f", &nota);
-    
+    do {
+        printf("Digite uma nota entre 0 e 10: ");
+        scanf("%f", &nota);
+        if(nota<0 || nota>10){
+            printf("Nota invalida, digite novamente\n");
+        }
+    } 
+    while(nota<0 || nota>10);
+    printf("Nota registrada: %.2f\n", nota);
+    return 0;
 }
