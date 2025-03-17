d0774bd69c928837dd5b72f9f62e2b98e8bc0137
pedrinndias <pedro3soares@gmail.com>
2025-03-17 14:51:27 -0300

    Questão 5 -Senos

---
+++ aulapratica2_lista2_codigo5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float seno1, seno2;
+    seno1=sin(3,14);
+    seno2=sin(4,13);
+    printf("Seno de 3,14: %2f\n", seno1);
+    printf("Seno de 4,13: %2f\n", seno2);
+    return 0;
+}
