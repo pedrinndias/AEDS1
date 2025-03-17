23be41dedf8ed2244a01c2d934e856a7c381d6e0
pedrinndias <pedro3soares@gmail.com>
2025-03-17 19:15:56 -0300

    correção

--- aulapratica4_lista4_codigo14.c
+++ aulapratica4_lista4_codigo14.c
@@ -4,14 +4,17 @@ int main(){
     float soma=0;
     printf("Escreva um numero inteiro: ");
     scanf("%d", &n);
+    if(soma==0){
+        printf("O valor da soma e: 0\n");
+    } 
     if(n==1){
         soma=1;
     } else if(n>=2){
         for (int i = 1; i <= n; i++){
         denominador = i+1;
-        soma = 1+1/denominador;
     }
     }
-    printf("O valor da soma e: %f\n", soma);
+    soma = 1+1/denominador;
+    printf("O valor da soma e: %2.f\n", soma);
     return 0;
 }
