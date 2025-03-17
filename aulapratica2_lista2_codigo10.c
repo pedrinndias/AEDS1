890dc68366c2b16b04000eb4200b17a5c26c4e64
pedrinndias <pedro3soares@gmail.com>
2025-03-17 16:18:40 -0300

    Questão 10 - pitagoras

---
+++ aulapratica2_lista2_codigo10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int cat1, cat2;
+    float hip;
+    printf("Digite os valores dos catetos: ");
+    scanf("%d %d", &cat1, &cat2);
+    hip = sqrt(pow(cat1,2)+pow(cat2,2));
+    printf("O valor da hipotenusa e: %f\n", hip);
+    return 0;
+}
