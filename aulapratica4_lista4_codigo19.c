f234876a2040cc8636f7d9d19cbe9dbac9f593d6
pedrinndias <pedro3soares@gmail.com>
2025-03-17 21:34:05 -0300

    Questão 19 - Plano cartesiano

---
+++ aulapratica4_lista4_codigo19.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    float x, y;
+    if(x==0 && y==0){
+        printf("O ponto (%.2f, %.2f) esta na origem\n", x, y);
+    } 
+   do{
+         printf("Digite o valor de x: ");
+             scanf("%f", &x);
+         printf("Digite o valor de y: ");
+            scanf("%f", &y);
+     if(x>0 && y>0){
+         printf("O ponto (%.2f, %.2f) esta no primeiro quadrante\n", x, y);
+    } else if(x<0 && y>0){
+         printf("O ponto (%.2f, %.2f) esta no segundo quadrante\n", x, y);
+    } else if(x<0 && y<0){
+         printf("O ponto (%.2f, %.2f) esta no terceiro quadrante\n", x, y);
+    } else if(x>0 && y<0){
+         printf("O ponto (%.2f, %.2f) esta no quarto quadrante\n", x, y);
+    } 
+    }
+    while(x==0 && y==0);
+    return 0;
+}
