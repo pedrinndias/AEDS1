22cc68304146d7e493dfd2029871575c016e1c4e
pedrinndias <pedro3soares@gmail.com>
2025-03-19 20:46:26 -0300

    reduzir casas decimais

--- aulapratica2_lista2_codigo10.c
+++ aulapratica2_lista2_codigo10.c
@@ -6,6 +6,6 @@ int main(){
     printf("Digite os valores dos catetos: ");
     scanf("%d %d", &cat1, &cat2);
     hip = sqrt(pow(cat1,2)+pow(cat2,2));
-    printf("O valor da hipotenusa e: %f\n", hip);
+    printf("O valor da hipotenusa e: %.2f\n", hip);
     return 0;
 }
