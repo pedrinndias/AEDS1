de5f58d3270d6e52f8af1b1d7e451a5b7485469e
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-21 07:47:22 -0300

    correção

--- aulapratica5_lista5_codigo2.c
+++ aulapratica5_lista5_codigo2.c
@@ -1,15 +1,24 @@
 #include<stdio.h>
-vois mostrarparemordemdecrescente(int n){
-for(int i=n; i>0; i--){
-resultado+=2;
+int par(int n){
+    int resultado=0;
+        for(int i=1; i<=n; i++){
+            resultado+=2;
 }
 return resultado;
 }
+void mostrarparemordemdecrescente(int n){
+    int valor;
+        for(int i=n; i>0; i--){
+            valor=par(i);
+            if(valor<n)
+                printf("%d" valor);
+}
+    return resultado;
+}
 int main(){
     int n_esimo, termo;
-    printf("Escreva numero n:");
-    scanf("%d" &n_esimo);
-    termo= par(n_esimo);
-    printf("o n-esimo termo eh %d", termo);
-    return 0;
+        printf("Escreva numero n:");
+        scanf("%d" &n_esimo);
+        mostrarparemordemdecrescente(n_esimo)
+        return 0;
 }
