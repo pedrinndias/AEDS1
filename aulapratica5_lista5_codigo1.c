1521829e2e5ab95da2a32fb7915788e65e0049f9
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-21 07:41:43 -0300

    Correção

--- aulapratica5_lista5_codigo1.c
+++ aulapratica5_lista5_codigo1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-int par(int n, int resultado){
-for(int i=0; i<=n; i++){
+int par(int n){
+    int resultado=0;
+for(int i=1; i<=n; i++){
 resultado+=2;
 }
 return resultado;
@@ -9,7 +10,7 @@ int main(){
     int n_esimo, termo;
     printf("Escreva numero n:");
     scanf("%d", &n_esimo);
-    par(n_esimo);
-    printf("o n-esimo termo eh %d", par);
+    termo = par(n_esimo);
+    printf("o n-esimo termo eh %d", termo);
     return 0;
 }
