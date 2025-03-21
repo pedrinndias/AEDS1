551e8116d30619bf5c2279dc30f260f597478bff
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-21 07:37:59 -0300

    correção

--- aulapratica5_lista5_codigo1.c
+++ aulapratica5_lista5_codigo1.c
@@ -9,7 +9,7 @@ int main(){
     int n_esimo, termo;
     printf("Escreva numero n:");
     scanf("%d", &n_esimo);
-    termo= par(n_esimo);
-    printf("o n-esimo termo eh %d", termo);
+    par(n_esimo);
+    printf("o n-esimo termo eh %d", par);
     return 0;
 }
