6f17ae47910fabfa292619812d95ddfaa0e1e131
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-26 14:39:37 -0300

    Multiplo de 5

--- Lista 3/aulapratica3_lista3_codigo1.c
+++ Lista 3/aulapratica3_lista3_codigo1.c
@@ -1,1 +1,10 @@
-// Eu preciso iniciar o codigo
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Escreva um numero inteiro:");
+    scanf("%d", &num);
+    if (num % 5 == 0){
+    printf("Multiplo de 5");
+    }
+return 0;
+}
