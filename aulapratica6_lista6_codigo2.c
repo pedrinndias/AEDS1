4bf3ca083dc18d5a845c3c3dc2729c929263df0e
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-28 08:16:40 -0300

    correção

--- aulapratica6_lista6_codigo2.c
+++ aulapratica6_lista6_codigo2.c
@@ -8,7 +8,7 @@ int main(){
     int val1, val2, soma=0, produto=1;
     printf("Escreva dois valores inteiros: ");
     scanf("%d%d", &val1, &val2);
-    void calculaOperacoes (val1, val2);
+    calculaOperacoes (val1, val2, &soma, &produto);
     printf("A soma eh: %d", soma);
     printf("O produto eh: %d", produto);
     return 0;
