3644fdbd69f8f1469c9816593c968413bd9ed6ac
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-28 07:23:35 -0300

    Questão 1 - Lista 6

---
+++ Lista 6/aulapratica6_lista6_codigo1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+    void (int *a , int *b){
+        int aux;
+        *a = aux;
+        *a = *b;
+        *b = aux;
+        return;
+    }
+    int main(){
+        int a, b;
+        printf("Escreva dois valores inteiros: ");
+        scanf("%d%d" &a, &b);
+        void()
+        printf("Os valores trocados sao: %d, %d" a, b);
+        return 0;
+    }
