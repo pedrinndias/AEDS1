b090fdafa9a437ce6eaac8188d46877edc094f44
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-28 08:29:55 -0300

    Questão 3

---
+++ aulapratica6_lista6_codigo3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void atualizaNota(float *nota){
+    if(*nota<=9.5){
+        *nota+=0.5;
+    } else {
+        *nota = *nota;
+    }
+    return;
+}
+int main(){
+    float nota;
+    printf("Escreva sua nota: ");
+    scanf("%f", &nota);
+    atualizaNota(&nota);
+    printf("Sua nota atualizada eh: %.2f", nota)
+    return 0;
+}
