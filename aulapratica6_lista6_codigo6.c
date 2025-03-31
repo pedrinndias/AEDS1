e726e3c41acbc1f38998425e3fd1a7529362c2f7
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-31 10:01:41 -0300

    Questão 6 - Converter Tempo

---
+++ aulapratica6_lista6_codigo6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void converteTermpo(int segundos, int*horas, int*minutos, int*segundosRestantes){
+    *horas=minutos/60;
+    *minutos=segundos/60;
+    *segundosRestantes=segundos%60;
+    return;
+}
+int main(){
+    int segundos, horas=0, minutos=0, segundosRestantes=0;
+    printf("Escreva o tempo em segundos: ");
+    scanf("%d", &segundos);
+    converteTermpo(segundos, &horas, &minutos, &segundosRestantes);
+   printf("O tempo eh: %d:%d:%d\n", horas, minutos, segundosRestantes);
+    return 0;
+}
