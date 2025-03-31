9a629b0861639b9fde432ec04a73a8050d35dd80
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-31 10:59:57 -0300

    Questão 1 avaliativa

---
+++ aulapratica6_lista6_codigo1avaliativo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int media(*n, *quantN){
+    int seq;
+    float media;
+    seq+=*n;
+    media =seq/quantN;
+    return media;
+}
+int main(){
+    int n=1, quantImpares, quantN;
+    float mediaf;
+    for(int i=0; n!= 0; i++){
+        printf("Escreva um numero: ");
+        scanf("%d", &n);
+        if ((n%2) !== 0){
+        quantImpares++;
+        }
+        if (n!=0){
+        quantN++;
+        }
+    mediaf = media(&n, &quantN);
+    }
+    printf("A media dos numeros eh: %f\n", mediaf);
+    printf("A quantidade de numeros impares eh: %d\n", quantImpares);
+    return 0;   
+}
