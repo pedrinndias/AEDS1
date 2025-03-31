d0207de65da9af12537934fcfa057e659813580e
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-31 16:40:12 -0300

    correção

--- aulapratica6_lista6_codigo5avaliativo.c
+++ aulapratica6_lista6_codigo5avaliativo.c
@@ -1,16 +1,17 @@
     #include <stdio.h>
     #include <math.h>
-    void calculaPotencias(int n, int *quadrado, int *cubo){
+    void calculaPotencias(int n, double *quadrado, double *cubo){
         *quadrado=pow(n,2);
         *cubo=pow(n,3);
         return;
     }
     int main(){
-        int numero, quadrado=0, cubo=0;
+        int numero;
+        double quadrado=0, cubo=0;
         printf("Escreva um numero: ");
         scanf("%d", &numero);
         calculaPotencias(numero, &quadrado, &cubo);
-        printf("O quadrado de %d eh: %d\n", numero, quadrado);
-        printf("O cubo de %d eh: %d\n", numero, cubo);
+        printf("O quadrado de %d eh: %.0f\n", numero, quadrado);
+        printf("O cubo de %d eh: %.0f\n", numero, cubo);
         return 0;
     }
