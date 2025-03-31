76acfe4cb5d3296602478b327c49b5640c12bf92
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-31 11:42:24 -0300

    correção

--- aulapratica6_lista6_codigo1avaliativo.c
+++ aulapratica6_lista6_codigo1avaliativo.c
@@ -5,21 +5,17 @@ int main() {
     do {
         printf("Escreva um numero: ");
         scanf("%d", &numero);
-        
         if (numero != 0) {
             soma += numero;
-            quantN++;
-            
-            if (numero % 2 != 0) {
+            quantN++; 
+        if (numero % 2 != 0) {
                 quantImpares++;
-            }
+         }
         }
     } while (numero != 0);
     if (quantN > 0) {
         printf("A media dos numeros eh: %f\n", soma / quantN);
-    } else {
-        printf("Nenhum número foi inserido.\n");
     }
-    printf("A quantidade de numeros impares eh: %d\n", quantImpares);
+        printf("A quantidade de numeros impares eh: %d\n", quantImpares);
     return 0;
 }
