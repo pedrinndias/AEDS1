1de05a16cb6980fbfd8a2c2eced537300b9cbe66
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-31 11:56:28 -0300

    teste

---
+++ aulapratica6_lista6_codigo2avaliativo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void linha(int n,){
+    while (n>0){
+        printf("%d ", n);
+        n--;
+    }
+    return;
+}
+int main(){
+    int num;
+    printf("Escreva um numero: ");
+    scanf("%d", &num);
+    for (inti=n; i>=0; i--){
+        printf(" linha(i)!) \n", i);
+    }
+return 0;
+}
