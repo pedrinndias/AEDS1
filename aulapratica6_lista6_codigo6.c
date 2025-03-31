6a2a8b13867d6de796bc55458560edbf9ccc5695
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-31 10:06:30 -0300

    correção

--- aulapratica6_lista6_codigo6.c
+++ aulapratica6_lista6_codigo6.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void converteTermpo(int segundos, int*horas, int*minutos, int*segundosRestantes){
-    *horas= *minutos/60;
-    *minutos=(segundos/60)%60;
+    *horas=segundos/3600;
+    *minutos=segundos/60;
     *segundosRestantes=segundos%60;
     return;
 }
