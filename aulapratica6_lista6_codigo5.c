d6692b8ad7acf6e2a6d45e462d3345fc7e68ec14
Pedro Dias Soares <pedro3soares@gmail.com>
2025-03-31 09:55:24 -0300

    Questão 5 - fatorial

---
+++ aulapratica6_lista6_codigo5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void fatorial(int n, int*resultado){
+    for(int i=1, i<=n; i++){
+        *resultado*=i;
+    }
+    return;
+}
+int main(){
+    int n, resultado=1;
+    printf("Escreva um numero: ");
+    scanf("%d", &n);
+    fatorial(n, &resultado);
+    printf("O fatorial de %d eh: %d\n", n, resultado);
+    return 0;
+}
