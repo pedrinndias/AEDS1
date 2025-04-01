deb6581d1e2ac3a4e7ec2f592491aacbfd8ceedb
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-01 19:06:48 -0300

    teste

--- prova_prova1passada_codigo2.c
+++ prova_prova1passada_codigo2.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main(){
-    int n, positivo=0, negativo
-
+    int n, positivo=0, negativo=0;
+    do{
+    printf("Escreva um numero inteiro: ");
+    scanf("%d", &n);
+    if(n>0){
+        positivo++;
+    } else if(n<0){
+        negativo++;
+    }
+    }while(n!=0);
+    printf("Quantidade de numeros positivos: %d\n", positivo);
+    printf("Quantidade de numeros negativos: %d\n", negativo);
+    return 0;
 }
