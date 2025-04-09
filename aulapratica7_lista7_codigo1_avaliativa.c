7c0d6ce4130aa4c567786e350ed1682b23d870dc
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-09 08:14:39 -0300

    teste

---
+++ aulapratica7_lista7_codigo1_avaliativa.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){ 
+    int n, multi=3;
+    printf("Escreva um numero inteiro: \n");
+    scanf(%"%d", &n);
+    for(int i=1; i <= n, i++){
+        printf("%d\n",  multi*1);
+}
+return 0;
+}
