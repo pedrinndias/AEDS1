16bb0d5974da5e94ad5c167e8109196c1c716285
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-09 19:14:47 -0300

    correção

---
+++ aulapratica7_lista7_codigo6_avaliativa.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int soma(int n){
+    int soma=0;
+    for(int i = 1; i<=soma; i++){
+        soma += 1/i;
+    }
+    return soma;
+}
+int main(){
+    int num;
+    printf("Digite um numero inteiro positivo: ");
+    scanf("%d", &num);
+    printf("A soma eh: %d\n", soma(num));
+    return 0;
+}
