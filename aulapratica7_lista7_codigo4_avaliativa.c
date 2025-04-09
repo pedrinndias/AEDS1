cb29aa2a86f89ef2f27db26365b8c9110bbd6a29
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-09 18:36:23 -0300

    teste

---
+++ aulapratica7_lista7_codigo4_avaliativa.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void cont_par(n, *pares){
+    if(n%2==0 && n!=0){
+        (*pares)++;
+    }
+    return;
+}
+int main(){
+    int n, pares=0;
+    do{ 
+        printf("Digite um numero inteiro positivo (0 para sair): ");
+        scanf("%d", &n);
+        cont_par(n, &pares);
+    } while (n!=0);
+    printf("%d numeros pares", pares );
+    return 0;
+}
