9f6c0d59a30ebd7aa813c5a90d6e5bf228178b61
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-09 18:59:00 -0300

    correção

---
+++ aulapratica7_lista7_codigo5_avaliativa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int maior(int *num1, *num2){
+    int maior;
+    maior= (*num1) > (*num2) ? (*num1) : (*num2);
+    return maior;
+}
+int main(){
+    int n1, n2;
+    printf("Digite dois numeros inteiros: ");
+    scanf("%d %d", &n1, &n2);
+    printf("O maior numero eh: %d\n", maior(&n1, &n2));
+    return 0;
+}
