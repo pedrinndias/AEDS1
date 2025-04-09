b55831b7b8b2e2f9105b64c98257c661cbcd0727
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-09 20:37:16 -0300

    teste

---
+++ aulapratica7_lista7_codigo10_avaliativa.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int pertenceFibonacci(int num) {
+    int a = 0, b = 1, c;
+    if (num == 0 || num == 1) {
+        return 1;
+    }
+    while (b <= num) {
+        c = a + b;
+        a = b;
+        b = c;
+        if (b == num) {
+            return 1;
+        }
+    }
+    return 0;
+}
+int main() {
+    int numero;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+    if (pertenceFibonacci(numero)) {
+        printf("%d pertence a sequencia de Fibonacci.", numero);
+    } else {
+        printf("%d não pertence a sequencia de Fibonacci.", numero);
+    }
+    return 0;
+}
