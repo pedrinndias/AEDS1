a8184ff32f55d57f6f68b61ba2b80a9fbf1b0e45
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-25 07:55:02 -0300

    teste

---
+++ aulapratica9_lista9_codigo1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void buscvetor (int vetor[], int num){
+    int verificador;
+    for (int i=0; i<20; i++)
+    if (num == vetor[i]){
+        printf ("O numero aparece na posicao: %d\n" vetor[i]);
+        verificador++;
+    }
+    if (verificador == 0)
+        printf("O numero nao aparece no intervalo")
+    return;
+}
+
+int main{
+    int vetor[20];
+    int num;
+    srand (time(NULL));
+    for (int i=0; i<20; i++){
+        vetor[i]= rand()/100 + 1;
+    }
+    printf("Escreva um numero para ser buscado");
+    scanf("%d", &num);
+    buscvetor (&vetor,num)
+}
