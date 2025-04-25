e307a12a2692bd426373c0bdc2e9f2b69ba85f19
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-25 08:00:08 -0300

    teste

--- aulapratica9_lista9_codigo1.c
+++ aulapratica9_lista9_codigo1.c
@@ -19,9 +19,9 @@ int main(){
     int num;
     srand (time(NULL));
     for (int i=0; i<20; i++){
-        vetor[i]= rand()/100 + 1;
+        vetor[i]=(rand()/100)+1;
     }
-    printf("Escreva um numero para ser buscado");
+    printf("Escreva um numero para ser buscado:\n");
     scanf("%d", &num);
-    buscvetor (vetor[],num)
+    buscvetor (vetor,num);
 }
