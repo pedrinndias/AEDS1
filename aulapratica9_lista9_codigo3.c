665f2b2648a1c6bb176c14fc84eee2f2732a8fda
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-30 19:36:26 -0300

    teste

---
+++ aulapratica9_lista9_codigo3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void preencherVetor(int vetor[]) {
+    for (int i = 0; i < 5; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &vetor[i]);
+    }
+}
+int encontrarPosicaoMenor(int vetor[]) {
+    int posMenor = 0;
+    int menorValor = vetor[0];
+    for (int i = 1; i < 5; i++) {
+        if (vetor[i] < menorValor) {
+            menorValor = vetor[i];
+            posMenor = i;
+        }
+    }
+    return posMenor;
+}
+void trocarEImprimir(int vetor[], int posMenor) {
+    if (posMenor != 0) {
+        int temp = vetor[0];
+        vetor[0] = vetor[posMenor];
+        vetor[posMenor] = temp;
+    }
+    printf("\nVetor apos trocar o menor elemento (%d) com o primeiro:\n", vetor[0]);
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+}
+int main() {
+    int meuVetor[5];
+    int posicaoDoMenor;
+    preencherVetor(meuVetor, 5);
+    posicaoDoMenor = encontrarPosicaoMenor(meuVetor, 5);
+    trocarEImprimir(meuVetor, 5, posicaoDoMenor);
+    return 0;
+}
