9705c48854350e936cdf7f28b99c4dd93cb6f2d6
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-30 21:04:09 -0300

    correção

--- aulapratica9_lista9_codigo3.c
+++ aulapratica9_lista9_codigo3.c
@@ -31,8 +31,8 @@ void trocarEImprimir(int vetor[], int posMenor) {
 int main() {
     int meuVetor[5];
     int posicaoDoMenor;
-    preencherVetor(meuVetor[], 5);
-    posicaoDoMenor = encontrarPosicaoMenor(meuVetor[]);
-    trocarEImprimir(meuVetor[], posicaoDoMenor);
+    preencherVetor(&meuVetor[]);
+    posicaoDoMenor = encontrarPosicaoMenor(&meuVetor[]);
+    trocarEImprimir(&meuVetor[], posicaoDoMenor);
     return 0;
 }
