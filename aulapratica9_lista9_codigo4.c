72ccc20c2a7be881ad1918c9bd88ea7715661f4d
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-30 21:25:38 -0300

    teste

--- aulapratica9_lista9_codigo4.c
+++ aulapratica9_lista9_codigo4.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-int subvetores(int A[], int B[], int C[]){
-    for (int i=0; i<10; i++){
+void subvetores(int A[], int B[], int C[]) {
+    for (int i = 0; i < 10; i++) {
         C[i] = A[i] - B[i];
-        return C[i];
     }
-    return 0;
 }
-int main(){
+int main() {
     int A[10];
     int B[10];
     int C[10];
-    for (int i=0; i<10; i++){
-        printf("Digite o %d elemento do vetor A: ", i+1);
+    for (int i = 0; i < 10; i++) {
+        printf("Digite o %d elemento do vetor A: \n", i + 1);
         scanf("%d", &A[i]);
     }
-    for (int i=0; i<10; i++){
-        printf("Digite o %d elemento do vetor B: ", i+1);
+    for (int i = 0; i < 10; i++) {
+        printf("Digite o %d elemento do vetor B: \n", i + 1);
         scanf("%d", &B[i]);
     }
-    for (int i=0; i<10; i++){
-        C[i] = subvetores(A, B, C);
-        printf("O %d elemento do vetor C eh: %d\n", i+1, C[i]);
+    subvetores(A, B, C);
+    for (int i = 0; i < 10; i++) {
+        printf("O %d elemento do vetor C eh: %d\n", i + 1, C[i]);
     }
     return 0;
 }
