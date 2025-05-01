b6499809f6a3381c179e42ae3ef9a534de9fa160
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-30 21:42:27 -0300

    teste

--- aulapratica9_lista9_codigo5.c
+++ aulapratica9_lista9_codigo5.c
@@ -21,7 +21,7 @@ int main() {
     preencheValores(vetor);
     copiaNegativos(vetor, vetorNegativos, contador);
     printf("Os valores negativos do vetor sao:\n");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 11; i++) {
         if (vetorNegativos[i] < 0) {
             printf("%d ", vetorNegativos[i]);
         }
