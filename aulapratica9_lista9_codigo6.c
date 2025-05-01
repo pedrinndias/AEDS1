fc4b8f2d9c51f34194a93f9890a35622e7233f58
Pedro Dias Soares <pedro3soares@gmail.com>
2025-04-30 22:02:13 -0300

    teste

---
+++ aulapratica9_lista9_codigo6.c
@@ -0,0 +1,41 @@
+#include <stdio.h> 
+#define LINHAS 2
+#define COLUNAS 3
+int main() {
+    int matriz[LINHAS][COLUNAS];
+    int matrizTransposta[COLUNAS][LINHAS];
+    int soma = 0;
+    printf("Digite os valores para preencher a matriz %dx%d:\n", LINHAS, COLUNAS);
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            printf("Digite o valor para a posição [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    printf("\nMatriz original:\n");
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            matrizTransposta[j][i] = matriz[i][j];
+        }
+    }
+    printf("\nMatriz transposta:\n");
+    for (int i = 0; i < COLUNAS; i++) {
+        for (int j = 0; j < LINHAS; j++) {
+            printf("%d ", matrizTransposta[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            soma += matriz[i][j];
+        }
+    }
+    printf("\nA soma dos elementos da matriz é: %d\n", soma);
+    return 0;
+}
