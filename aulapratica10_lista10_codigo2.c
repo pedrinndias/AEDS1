8f15f8dda35695961423391964b7745cee0d7051
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-02 08:09:53 -0300

    codigo

--- aulapratica10_lista10_codigo2.c
+++ aulapratica10_lista10_codigo2.c
@@ -3,8 +3,8 @@ int main(){
     char frase[100];
     printf("Digite uma frase:");
     scanf("%[^\n]", frase);
-    for (int i=0; i<4; i++){
-        printf("%c", frase[i])
+    for (int i=0; i<5; i++){
+        printf("%c\n", frase[i]);
     }
     return 0;
 }
