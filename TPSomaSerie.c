af51a6ce30526836569573ee10bb8cc45eff54d5
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-13 17:41:49 -0300

    Teste

---
+++ TPSomaSerie.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int val;
+    float seq = 0, den = 1, num = 1;
+    scanf("%d", &val);
+    for (int i = 1; i < val; i++){
+        seq += num / den;
+        num += 2;
+        den++;
+    }
+    printf("%.2f\n", seq);
+    return 0;
+}
