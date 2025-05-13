c0f68e9c0fe5993a00cd86ba6ab36afec8705000
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-13 17:44:36 -0300

    correção

--- TPSomaSerie.c
+++ TPSomaSerie.c
@@ -3,10 +3,10 @@ int main(){
     int val;
     float seq = 0, den = 1, num = 1;
     scanf("%d", &val);
-    for (int i = 1; i < val; i++){
+    for (int i = 1; i <= val; i++){
         seq += num / den;
         num += 2;
-        den++;
+        den ++;
     }
     printf("%.2f\n", seq);
     return 0;
