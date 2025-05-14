df62b361e483f1d1d94f9958c6afcce6610c8a21
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-14 08:51:59 -0300

    Correção

--- TPPrimoVersa2.c
+++ TPPrimoVersa2.c
@@ -1,32 +1,20 @@
 #include <stdio.h>
-int verificadorP(int i){
-    if (i <= 1) {
-        return 0;
+
+int verificadorP(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
     }
-    int cont = 0;
-    for (int j = 1; j <= i; j++){
-        if (i % j == 0){
-            cont++;
-        }
-    }
-    if (cont == 2){
-        return 1;
-    }
-    else {
-        return 0;
-    }
+    return 1;
 }
-int main(){
-    int cont=0, menor, maior, primo[maior - menor + 1];
+
+int main() {
+    int menor, maior;
     scanf("%d %d", &menor, &maior);
-    for (int i = menor; i <= maior; i++){
-        if (verificadorP(i) == 1){
-            primo[cont] = i;
-            cont++;
+    for (int i = menor; i <= maior; i++) {
+        if (verificadorP(i)) {
+            printf("%d ", i);
         }
-    }
-    for (int i = 0; i < cont; i++){
-        printf("%d ", primo[i]);
     }
     return 0;
 }
