7f275a2d90fdc2264e139a6e55d74b89b8a16e6d
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-14 09:15:02 -0300

    Teste

---
+++ contagem_caracteres_maiúsculos.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int contarMaiusculas(char *str) {
+    int contador = 0; 
+    for (int i = 0; str[i] != '\0'; i++) {
+
+        if (isupper(str[i])) {
+            contador++;
+        }
+    }
+    return contador; 
+}
+
+int main() {
+    char linha[1024];
+    while (fgets(linha, sizeof(linha), stdin) != NULL) {
+        linha[strcspn(linha, "\n")] = '\0';
+
+        if (strcmp(linha, "FIM") == 0) {
+            printf("\n",);
+            break;
+        }
+
+        // Chama a função para contar os caracteres maiúsculos na linha atual [1]
+        int maiusculas = contarMaiusculas(linha);
+    
+        printf("%d\n", maiusculas);
+
+    }
+    return 0;
+}
+
