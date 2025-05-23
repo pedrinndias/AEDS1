7443ccad1d8eeee9ef7eb0450dad3e07724fbe90
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-23 19:19:14 -0300

    codigo corrigido

--- codigo3_recursividade2.c
+++ codigo3_recursividade2.c
@@ -4,32 +4,39 @@
 int ehprimo(int n) {
     if (n <= 1) {
         return 0;
-    }
-    for (int i = 2; i <= n ; i++) {
-        if (n % i == 0) {
-            return 0;
-        }
-    } 
-    return 1;
-}
-int digPprimos (int **matriz, int linhas) {
-    int cont = 0;
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < linhas; j++) {
-            if (i == j) {
-                if (ehprimo(matriz[i][j])) {
-                    cont++;
+    } else { 
+        if (n == 2) {
+        return 1;
+        } else { 
+            if (n % 2 == 0) {
+                return 0;
+            } else { 
+                for (int i = 3; i < n ; i++) {
+                    if (n % i == 0) {
+                        return 0;
+                    }
                 }
+                return 1;}
+        }
+    }
+}
+int digPprimos(int **matriz, int linhas) {
+    int cont = 0;
+    for (int i = 0; i < linhas; i++) { 
+        if (ehprimo(matriz[i][i])) {    
+            cont++;
         }
     }
     return cont;
-}
 }
 int main() {
     int linhas;
     printf("Digite o tamanho da matriz: ");
     scanf("%d", &linhas);
     int **matriz = (int **)malloc(linhas * sizeof(int *));
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = (int *)malloc(linhas * sizeof(int));
+    }
     for (int i=0; i<linhas; i++){
         for (int j=0; j<linhas; j++){
             printf("Digite o elemento [%d][%d]: ", i, j);
