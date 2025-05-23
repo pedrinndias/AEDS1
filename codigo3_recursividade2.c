c6122c7d4214e445d00dfae1040ed4f53b20e0bc
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-23 17:26:14 -0300

    teste

--- codigo3_recursividade2.c
+++ codigo3_recursividade2.c
@@ -14,9 +14,13 @@ int ehprimo(int n) {
 int digPprimos (int **matriz, int linhas) {
     int cont = 0;
     for (int i = 0; i < linhas; i++) {
-            if (ehprimo(matriz[i][i])) {
-                cont++;
-            }  
+        for (int j = 0; j < linhas; j++) {
+            if (i == j) {
+                if (ehprimo(matriz[i][j])) {
+                    cont++;
+                }
+        }
+    }
     return cont;
 }
 }
@@ -24,7 +28,7 @@ int main() {
     int linhas;
     printf("Digite o tamanho da matriz: ");
     scanf("%d", &linhas);
-    int **matriz = (int **)malloc(linhas * sizeof(int));
+    int **matriz = (int **)malloc(linhas * sizeof(int *));
     for (int i=0; i<linhas; i++){
         for (int j=0; j<linhas; j++){
             printf("Digite o elemento [%d][%d]: ", i, j);
