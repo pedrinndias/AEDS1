93ed82e9cd76e31d4df4fe2369c75ff7c3d3bb16
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-24 09:45:21 -0300

    teste

---
+++ Estudo-Prova2/Lista 8 - Revisão/codigo3_lista8_revisao.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define TAM 5
+int main(){
+    int *vetor1, *vetor2;
+    vetor1 = (int*) malloc(TAM * sizeof(int));
+    vetor2 = (int*) malloc(TAM * sizeof(int));
+    if (vetor1 == NULL || vetor2 == NULL) {
+        printf("Memoria insuficiente\n");
+        return 1;
+    }
+    for (int i = 0; i < TAM; i++) {
+        printf("Digite o valor do vetor1[%d]: ", i);
+        scanf("%d", &vetor1[i]);
+        printf("Digite o valor do vetor2[%d]: ", i);
+        scanf("%d", &vetor2[i]);
+    }
+    int *vetorSoma;
+    vetorSoma = (int*) calloc(TAM * sizeof(int));
+    if (vetorSoma == NULL) {
+        printf("Memoria insuficiente\n");
+        return 1;
+    }
+    for (int i = 0; i < TAM; i++) {
+        vetorSoma[i] = vetor1[i] + vetor2[i];
+        printf("vetorSoma[%d] = %d\n", i, vetorSoma[i]);
+    }
+    free(vetor1);
+    free(vetor2);
+    free(vetorSoma);
+    return 0;
+}
