0110316c3468cb420072c09bc9e4d23ca8afd270
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-29 14:40:48 -0300

    correção

--- codigo5_listarevisao.c
+++ codigo5_listarevisao.c
@@ -18,18 +18,18 @@ int main(){
     printf("Digite uma string: ");
     scanf("%[^\n]", str);
     int cont = contTamanho(str);
-    char *strInv = (char *)malloc(cont * sizeof(char)); 
+    char *strInv = (char *)malloc((cont+1) * sizeof(char));//(cont+1) é para incluir o caractere nulo no final da string invertida
     if(strInv == NULL){
         printf("Memoria insuficiente!\n");
         free(str);
         return 1;
     }
-    strInv[cont] = '\0';
-    for(int i = 0; i < cont; i++){
-        strInv[i] = str[cont];
-        cont--;
+for (int i = 0; i < cont; i++) { // Loop para inverter a string
+        strInv[i] = str[cont - 1 - i];//(cont - 1) é o último índice da string original //(- i) é para inverter a ordem dos caracteres
     }
-    printf("String invertida: %s\n", strInv);
+    strInv[cont] = '\0';// Adiciona o caractere nulo ao final da string invertida
+    printf("String original: %s\n", str);// Exibe a string original
+    printf("String invertida: %s\n", strInv);// Exibe a string invertida
     free(str);
     free(strInv);
     return 0;
