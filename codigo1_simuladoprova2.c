2b9d8c645d62b97cb09044159024826008ef33dc
Pedro Dias Soares <pedro3soares@gmail.com>
2025-05-30 19:27:40 -0300

    final

--- codigo1_simuladoprova2.c
+++ codigo1_simuladoprova2.c
@@ -3,15 +3,12 @@
 #include<time.h>
 #define TAM 20
 int verificaRepeticao(int *vetor, int posicao){
-    if(posicao==0){
-        return *vetor + posicao;
-    }else{
-        if(*vetor+posicao==verificaRepeticao(vetor-1, posicao-1)){
+    for (int i = 0; i < posicao; i++) {
+        if (vetor[i] == vetor[posicao]) {
             return 1;
-    }else{
-            return 0;
         }
     }
+    return 0;
 }
 int main(){
     int vetor[TAM];
