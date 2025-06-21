5a0aa3bac390d3e3f8eb000d5abe38295a13873d
Pedro Dias Soares <pedro3soares@gmail.com>
2025-06-20 23:00:00 -0300

    correção

--- aulapratica13_lista13_codigo2.1.c
+++ aulapratica13_lista13_codigo2.1.c
@@ -1,1 +1,19 @@
-    
+#include<stdio.h>
+int main() {
+   FILE *fp;
+   fp = fopen("aulapratica12_lista12_codigo1.txt", "r");
+   if(fp == NULL) {
+       printf("Erro ao abrir o arquivo para leitura.\n");
+       return 1;
+   }
+   int contA=0;
+   char veri[100];
+   while(fgets(&veri,99, fp) != NULL) {
+       if(veri == 'A' || veri == 'a') {
+           contA++;
+       }
+   }
+   printf("O numero de letras A no arquivo e: %d\n", contA);
+   fclose(fp);
+   return 0;
+}
