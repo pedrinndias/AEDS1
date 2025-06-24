6df37a8c9c5355f9239fed4ce27c8be01af819f2
Pedro Dias Soares <pedro3soares@gmail.com>
2025-06-24 15:17:40 -0300

    Final

--- aulapratica14_lista14_codigo6.c
+++ aulapratica14_lista14_codigo6.c
@@ -1,28 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #define TAM 5
-typedef struct Banda{
+#define TOP_RANKING 3
+typedef struct Banda {
     char nome[50];
     char estiloMusical[30];
     int numeroIntegrantes;
     int posicaoRanking;
-}Banda;
-void imprimirDados(Banda *b, int i){
-    printf("*** Banda Cadastrada %d ***\n", i+1);
+} Banda;
+void imprimirDados(Banda *b, int i) {
+    printf("*** Banda Cadastrada %d ***\n", i + 1);
     printf("Nome: %s\n", b[i].nome);
     printf("Estilo Musical: %s\n", b[i].estiloMusical);
     printf("Numero de Integrantes: %d\n", b[i].numeroIntegrantes);
     printf("Posicao no Ranking: %d\n", b[i].posicaoRanking);
 }
-int main(){
+int main() {
     Banda *banda = (Banda *)malloc(TAM * sizeof(Banda));
     if (banda == NULL) {
         printf("Erro ao alocar memoria.\n");
         return 1;
     }
-    
-    for(int i = 0; i < TAM; i++){
-        printf("*** Preenchendo dados da banda %d ***\n", i+1);
+    for (int i = 0; i < TAM; i++) {
+        printf("*** Preenchendo dados da banda %d ***\n", i + 1);
         printf("Digite o nome: ");
         scanf(" %[^\n]", banda[i].nome);
         printf("Digite o estilo musical: ");
@@ -33,9 +34,8 @@ int main(){
         scanf("%d", &banda[i].posicaoRanking);
         printf("\n*** Dados preenchidos com sucesso! ***\n");
     }
-    
     printf("\n\n*** Dados das bandas ***\n");
-    for(int i = 0; i < TAM; i++){
+    for (int i = 0; i < TAM; i++) {
         imprimirDados(banda, i);
     }
     printf("*** Pergunta sobre as bandas cadastradas ***\n");
@@ -45,31 +45,61 @@ int main(){
     if (resposta == 'n' || resposta == 'N') {
         printf("Obrigado por participar!\n");
         free(banda);
+        banda = NULL;
         return 0;
     } else if (resposta == 's' || resposta == 'S') {
-        int *ordem=(char *)malloc(TAM * sizeof(char));
-        if (ordem == NULL) {
-            printf("Erro ao alocar memoria para a ordem.\n");
+        char **bandasPorRanking = (char **)malloc(TAM * sizeof(char *));
+        if (bandasPorRanking == NULL) {
+            printf("Erro ao alocar memoria para bandas por ranking.\n");
             free(banda);
+            banda = NULL;
             return 1;
         }
-        for(int i=0; i<TAM; i++){
-            for (int j=1; j<=TAM; j++){
-                if(banda[i].posicaoRanking == j){
-                    ordem[j-1] = banda[i].nome;
+        for (int i = 0; i < TAM; i++) {
+            bandasPorRanking[i] = (char *)malloc(50 * sizeof(char));
+            if (bandasPorRanking[i] == NULL) {
+                printf("Erro ao alocar memoria para o nome da banda.\n");
+                for (int k = 0; k < i; k++) {
+                    free(bandasPorRanking[k]);
                 }
+                free(bandasPorRanking);
+                free(banda);
+                banda = NULL;
+                return 1;
+            }
+            bandasPorRanking[i][0] = '\0';
+        }
+        for (int i = 0; i < TAM; i++) {
+            if (banda[i].posicaoRanking >= 1 && banda[i].posicaoRanking <= TAM) {
+                strcpy(bandasPorRanking[banda[i].posicaoRanking - 1], banda[i].nome);
             }
         }
-        char aux[50];
+        char nomeBandaFavorita[50];
         printf("Digite o nome da banda que voce quer verificar: ");
-        scanf(" %[^\n]", aux);
-        if (aux == ordem[0] || aux == ordem[1] || aux == ordem[2] || aux == ordem[3]) {
-            printf("Sua banda favorita foi cadastrada!\n");
-        } else {
-            printf("Sua banda favorita nao foi cadastrada.\n");
+        scanf(" %[^\n]", nomeBandaFavorita);
+
+        int encontrada = 0;
+        for (int i = 0; i < TOP_RANKING; i++) {
+            if (strcmp(nomeBandaFavorita, bandasPorRanking[i]) == 0) {
+                encontrada = 1;
+                break;
+            }
         }
-        free(ordem);
+
+        if (encontrada) {
+            printf("Sua banda favorita foi cadastrada e esta entre as %d primeiras posicoes!\n", TOP_RANKING);
+        } else {
+            printf("Sua banda favorita nao foi cadastrada ou nao esta entre as %d primeiras posicoes.\n", TOP_RANKING);
+        }
+
+        for (int i = 0; i < TAM; i++) {
+            free(bandasPorRanking[i]);
+            bandasPorRanking[i] = NULL;
+        }
+        free(bandasPorRanking);
+        bandasPorRanking = NULL;
     }
     free(banda);
+    banda = NULL;
     return 0;
 }
