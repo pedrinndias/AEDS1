75b93e5fee01ead34d8ab75021b9cd9776e9db39
Pedro Dias Soares <pedro3soares@gmail.com>
2025-07-02 08:00:50 -0300

    Final

--- questaoStruct01.c
+++ questaoStruct01.c
@@ -29,8 +29,9 @@ int main(){
     filme F[TAM];
     printf("***Cadastramento de Filmes:***\n");
     for(int i=0; i<TAM; i++){
+        fflush(stdin);
         printf("Filme [%d]:\n",i+1);
-        printf("Escreva o nome do filme:");
+        printf("Escreva o nome do filme: ");
         scanf("%[^\n]", F[i].titulo);
         printf("\nEscreva a duracao do filme em minutos: ");
         scanf("%d", &F[i].duracao);
@@ -42,44 +43,49 @@ int main(){
     int aux1;
     int j;
     do{
+        fflush(stdin);
         printf("\n1- Ver filmes cadastrados\n");
         printf("2- Saiir\n");
         scanf("%d", &aux);
         switch(aux){
             case 1:
-            do{
-                printf("1-Ver todos os filmes\n");
-                printf("2-Escolha o filme(numero)\n");
-                printf("3-Sair");
-                scanf("%d", &aux1);
-                switch(aux1){
-                    case 1:
-                        for(int i=0; i<TAM; i++){
-                            printf("\nFilme [%d]:\n",i+1);
-                            printf("O nome do filme: %s", F[i].titulo);
-                            printf("\nA duracao do filme em minutos: %d", F[i].duracao);
-                            printf("\nA data de estreia eh: %02d/%02d/%d\n", F[i].dataEstreia.dia, F[i].dataEstreia.mes, F[i].dataEstreia.ano);
-                        }
-                        break;
-                    case 2:
-                        printf("Escreva o numero de cadastro do filme: ");
-                        scanf("%d",&j);
-                        j--;
-                        printf("\nFilme [%d]:\n",j+1);
-                        printf("O nome do filme: %s", F[j].titulo);
-                        printf("\nA duracao do filme em minutos: %d", F[j].duracao);
-                        printf("\nA data de estreia eh: %02d/%02d/%d\n", F[j].dataEstreia.dia, F[j].dataEstreia.mes, F[j].dataEstreia.ano);
-                        break;
-                    case 3:
-                        return 0;
-                        break;
-                }
-            }while(aux1!=3);
-            break;
+
+                do{
+                    printf("1-Ver todos os filmes\n");
+                    printf("2-Escolha o filme(numero)\n");
+                    printf("3-Sair");
+                    scanf("%d", &aux1);
+                    switch(aux1){
+                        case 1:
+                            for(int i=0; i<TAM; i++){
+                                printf("\nFilme [%d]:\n",i+1);
+                                printf("O nome do filme: %s", F[i].titulo);
+                                printf("\nA duracao do filme em minutos: %d", F[i].duracao);
+                                printf("\nA data de estreia eh: %02d/%02d/%d\n", F[i].dataEstreia.dia, F[i].dataEstreia.mes, F[i].dataEstreia.ano);
+                            }
+                            break;
+                        case 2:
+                            printf("Escreva o numero de cadastro do filme: ");
+                            scanf("%d",&j);
+                            j--;
+                            printf("\nFilme [%d]:\n",j+1);
+                            printf("O nome do filme: %s", F[j].titulo);
+                            printf("\nA duracao do filme em minutos: %d", F[j].duracao);
+                            printf("\nA data de estreia eh: %02d/%02d/%d\n", F[j].dataEstreia.dia, F[j].dataEstreia.mes, F[j].dataEstreia.ano);
+                            break;
+                        case 3:
+                            return 0;
+                            break;
+                    }
+                }while(aux1!=3);
+                
+                break;
+
 
             case 2:
-            return 0;
-            break;
+                return 0;
+                
+                break;
         }
 
     }while(aux!=2);
